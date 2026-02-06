beb5f0165576a8f5941fc797de999d2d126c47e1
Yamin Einzali <yamineinzali54@gmail.com>
2026-02-06 08:43:07 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -193,7 +193,7 @@ function App() {
                         <p className="text-sm text-gray-400">Health Monitoring Application</p>
                       </div>
                     </div>
-                   <a href="https://github.com/yamineinzali54-star/my-ibs-app" target="_blank" rel="noreferrer">
+                   <a href="https://my-ibs-app-tmlcm69tapc2tsnraq4eid.streamlit.app/" target="_blank" rel="noreferrer">
                       <ExternalLink className="text-gray-400 hover:text-pink-500 cursor-pointer" size={24} />
 <                     /a>
                   </div>
